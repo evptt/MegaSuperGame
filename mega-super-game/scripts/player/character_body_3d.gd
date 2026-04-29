0aa82a06650ac105c4b8c1fa7bd5a638ad1b28d3
evptt <aleksandrshahmatov9@gmail.com>
2026-04-29 23:32:27 +0700

    Reworked test location, added movement and 3rd camera view, added assets

---
+++ mega-super-game/scripts/player/character_body_3d.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+@onready var camera_pivot: Node3D = $CameraPivot
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var mouseSensivity = 0.1
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouseSensivity))
+		camera_pivot.rotate_x(deg_to_rad(-event.relative.y * mouseSensivity))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("right", "left", "down", "up")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
